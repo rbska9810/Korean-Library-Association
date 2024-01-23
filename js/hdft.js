f57c282373082eaaf2ede1979e914808944cd759
ggyu's_ddak's <152961789+rbska9810@users.noreply.github.com>
2024-01-23 15:26:02 +0900

    Add files via upload

--- js/hdft.js
+++ js/hdft.js
@@ -20,14 +20,17 @@ trigger.addEventListener('click', function(){
   siteMap.classList.toggle('on');
 })
 /* siteMap-content */
-let titleParent = document.querySelectorAll('.title-parent');
-let siteMapContent = document.querySelectorAll('.site-map-item');
-let siteMapBtn = document.querySelectorAll('.site-map-btn');
-for (let i = 0; i < titleParent.length; i++) {
-  titleParent[i].addEventListener('click', function (e) {
-    for (let j = 0; j < titleParent.length; j++) {
+let titleParent = document.querySelectorAll('.title-parent'); //눌러지는 title부분
+let siteMapContent = document.querySelectorAll('.site-map-item'); //눌렀을때 등장하는 부분
+
+for(let i =0;i < titleParent.length; i++){
+  titleParent[i].addEventListener('click', function(e){
+    let titleParentRemove = document.querySelector('.title-parent.on');
+    for(let j = 0;j < titleParent.length; j++ ){
       titleParent[j].classList.remove('on');
+      titleParent[i].classList.add('on');
     };
-    titleParent[i].classList.add('on');
+    titleParentRemove.classList.remove('on');
   });
-}
+};
+
